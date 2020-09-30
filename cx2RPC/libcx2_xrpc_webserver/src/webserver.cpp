4b4a2414c9f2069cf9bc7a07bc4aab191197658d
Aaron Mizrachi <aaron@unmanarc.com>
2020-09-30 00:41:56 -0500

    WebServer Enhancements

--- cx2RPC/libcx2_xrpc_webserver/src/webserver.cpp
+++ cx2RPC/libcx2_xrpc_webserver/src/webserver.cpp
@@ -12,6 +12,8 @@ WebServer::WebServer()
     obj = nullptr;
     sessionsManager.startGC( SessionsManager::threadGC, &sessionsManager );
     useFormattedJSONOutput = true;
+    usingCSRFToken = true;
+    useHTMLIEngine = true;
     authenticator = nullptr;
     methodManagers = nullptr;
 }
@@ -22,28 +24,28 @@ WebServer::~WebServer()
 
 void WebServer::acceptMultiThreaded(Network::Streams::StreamSocket *listenerSocket, const uint32_t &maxConcurrentConnections)
 {
-    if (!methodManagers) throw std::runtime_error("Don't Accept XRPCWeb before setting some methodsmanager");
-    if (!authenticator) throw std::runtime_error("Don't Accept XRPCWeb before setting some authenticator");
+    if (!methodManagers) throw std::runtime_error("Don't Accept XRPC Web before setting some methodsmanager");
+    if (!authenticator) throw std::runtime_error("Don't Accept XRPC Web before setting some authenticator");
 
     obj = this;
     multiThreadedAcceptor.setAcceptorSocket(listenerSocket);
     multiThreadedAcceptor.setCallbackOnConnect(_callbackOnConnect,obj);
     multiThreadedAcceptor.setCallbackOnInitFail(_callbackOnInitFailed,obj);
-    multiThreadedAcceptor.setCallbackOnTimedOut(_callbackOnTimedOut,obj);
+    multiThreadedAcceptor.setCallbackOnTimedOut(_callbackOnTimeOut,obj);
     multiThreadedAcceptor.setMaxConcurrentClients(maxConcurrentConnections);
     multiThreadedAcceptor.startThreaded();
 }
 
 void WebServer::acceptPoolThreaded(Network::Streams::StreamSocket *listenerSocket, const uint32_t &threadCount, const uint32_t &threadMaxQueuedElements)
 {
-    if (!methodManagers) throw std::runtime_error("Don't Accept XRPCWeb before setting some methodsmanager");
-    if (!authenticator) throw std::runtime_error("Don't Accept XRPCWeb before setting some authenticator");
+    if (!methodManagers) throw std::runtime_error("Don't Accept XRPC Web before setting some methodsmanager");
+    if (!authenticator) throw std::runtime_error("Don't Accept XRPC Web before setting some authenticator");
 
     obj = this;
     poolThreadedAcceptor.setAcceptorSocket(listenerSocket);
     poolThreadedAcceptor.setCallbackOnConnect(_callbackOnConnect,obj);
     poolThreadedAcceptor.setCallbackOnInitFail(_callbackOnInitFailed,obj);
-    poolThreadedAcceptor.setCallbackOnTimedOut(_callbackOnTimedOut,obj);
+    poolThreadedAcceptor.setCallbackOnTimedOut(_callbackOnTimeOut,obj);
     poolThreadedAcceptor.setThreadsCount(threadCount);
     poolThreadedAcceptor.setTaskQueues(threadMaxQueuedElements);
     poolThreadedAcceptor.start();
@@ -62,30 +64,115 @@ bool WebServer::_callbackOnConnect(void * obj, Network::Streams::StreamSocket * 
     webHandler.setAuthenticators(webserver->getAuthenticator());
     webHandler.setSessionsManagger(webserver->getSessionsManager());
     webHandler.setUseFormattedJSONOutput(webserver->getUseFormattedJSONOutput());
+    webHandler.setUsingCSRFToken(webserver->getUsingCSRFToken());
     webHandler.setResourceFilter(webserver->getResourceFilter());
     webHandler.setResourcesLocalPath(webserver->getResourcesLocalPath());
+    webHandler.setWebServerName(webserver->getWebServerName());
+    webHandler.setSoftwareVersion(webserver->getSoftwareVersion());
+    webHandler.setUseHTMLIEngine(webserver->getUseHTMLIEngine());
 
-    // Handle the webservice.
-    Memory::Streams::Parsing::ParseErrorMSG err;
-    webHandler.parseObject(&err);
+    if (webserver->getExtCallBackOnConnect().call(obj,s,remotePairIPAddr,isSecure))
+    {
+        // Handle the webservice.
+        Memory::Streams::Parsing::ParseErrorMSG err;
+        webHandler.parseObject(&err);
+    }
     return true;
 }
 
-bool WebServer::_callbackOnInitFailed(void * obj, Network::Streams::StreamSocket *, const char * remotePairIPAddr, bool)
+bool WebServer::_callbackOnInitFailed(void * obj, Network::Streams::StreamSocket * s, const char * remotePairIPAddr, bool isSecure)
 {
-//    XRPC_WebServer * webserver = ((XRPC_WebServer *)obj);
+    WebServer * webserver = ((WebServer *)obj);
+    webserver->getExtCallBackOnInitFailed().call(obj,s,remotePairIPAddr,isSecure);
     return true;
 }
 
-void WebServer::_callbackOnTimedOut(void * obj, Network::Streams::StreamSocket *s, const char * remotePairIPAddr, bool)
+void WebServer::_callbackOnTimeOut(void * obj, Network::Streams::StreamSocket *s, const char * remotePairIPAddr, bool isSecure)
 {
-//    XRPC_WebServer * webserver = ((XRPC_WebServer *)obj);
-    // TODO: logs?
-    s->writeString("HTTP/1.1 503 Service Temporarily Unavailable\r\n");
-    s->writeString("Content-Type: text/html; charset=UTF-8\r\n");
-    s->writeString("Connection: close\r\n");
-    s->writeString("\r\n");
-    s->writeString("<center><h1>503 Service Temporarily Unavailable</h1></center><hr>\r\n");
+    WebServer * webserver = ((WebServer *)obj);
+    if (webserver->getExtCallBackOnInitFailed().call(obj,s,remotePairIPAddr,isSecure))
+    {
+        s->writeString("HTTP/1.1 503 Service Temporarily Unavailable\r\n");
+        s->writeString("Content-Type: text/html; charset=UTF-8\r\n");
+        s->writeString("Connection: close\r\n");
+        s->writeString("\r\n");
+        s->writeString("<center><h1>503 Service Temporarily Unavailable</h1></center><hr>\r\n");
+    }
+}
+
+bool WebServer::getUseHTMLIEngine() const
+{
+    return useHTMLIEngine;
+}
+
+void WebServer::setUseHTMLIEngine(bool value)
+{
+    useHTMLIEngine = value;
+}
+
+std::string WebServer::getWebServerName() const
+{
+    return webServerName;
+}
+
+void WebServer::setWebServerName(const std::string &value)
+{
+    webServerName = value;
+}
+
+std::string WebServer::getSoftwareVersion() const
+{
+    return softwareVersion;
+}
+
+void WebServer::setSoftwareVersion(const std::string &value)
+{
+    softwareVersion = value;
+}
+
+void WebServer::setSoftwareVersion(const uint32_t major, const uint32_t minor, const uint32_t subminor, const std::string & subText)
+{
+    setSoftwareVersion(std::to_string(major) + "." + std::to_string(minor) + "." + std::to_string(subminor) +  (subText.empty()? "" : (" " + subText))  );
+}
+
+void WebServer::setUsingCSRFToken(bool value)
+{
+    usingCSRFToken = value;
+}
+
+bool WebServer::getUsingCSRFToken() const
+{
+    return usingCSRFToken;
+}
+
+sWebServerCallBack WebServer::getExtCallBackOnTimeOut() const
+{
+    return extCallBackOnTimeOut;
+}
+
+sWebServerCallBack WebServer::getExtCallBackOnInitFailed() const
+{
+    return extCallBackOnInitFailed;
+}
+
+sWebServerCallBack WebServer::getExtCallBackOnConnect() const
+{
+    return extCallBackOnConnect;
+}
+
+void WebServer::setExtCallBackOnTimeOut(const sWebServerCallBack &value)
+{
+    extCallBackOnTimeOut = value;
+}
+
+void WebServer::setExtCallBackOnInitFailed(const sWebServerCallBack &value)
+{
+    extCallBackOnInitFailed = value;
+}
+
+void WebServer::setExtCallBackOnConnect(const sWebServerCallBack &value)
+{
+    extCallBackOnConnect = value;
 }
 
 std::string WebServer::getResourcesLocalPath() const
--- cx2RPC/libcx2_xrpc_webserver/src/webserver.h
+++ cx2RPC/libcx2_xrpc_webserver/src/webserver.h
@@ -12,34 +12,151 @@
 
 namespace CX2 { namespace RPC { namespace Web {
 
+struct sWebServerCallBack
+{
+    sWebServerCallBack()
+    {
+        callbackFunction=nullptr;
+    }
+
+    sWebServerCallBack( bool (*callbackFunction)(void *, Network::Streams::StreamSocket *, const char *, bool) )
+    {
+        this->callbackFunction=callbackFunction;
+    }
+
+    bool call(void *x, Network::Streams::StreamSocket *y, const char *z, bool q) const
+    {
+        if (!callbackFunction) return true;
+        return callbackFunction(x,y,z,q);
+    }
+    /**
+     * return false to cancel the connection, true to continue...
+     */
+    bool (*callbackFunction)(void *, Network::Streams::StreamSocket *, const char *, bool);
+};
+
+
 class WebServer
 {
 public:
     WebServer();
     ~WebServer();
-
-    void setObj(void *value);
-
+    /**
+     * @brief acceptMultiThreaded Start Web Server as Multi-Threaded (thread number grows as receive connections)
+     * @param listenerSocket Listener Prepared Socket (Can be TCP, TLS, etc)
+     * @param maxConcurrentConnections Max Number of allowed Connections/Threads
+     */
     void acceptMultiThreaded(Network::Streams::StreamSocket * listenerSocket, const uint32_t & maxConcurrentConnections = 10000);
-    void acceptPoolThreaded(Network::Streams::StreamSocket * listenerSocket, const uint32_t & threadCount = 20, const uint32_t & threadMaxQueuedElements = 1000 );
-
+    /**
+     * @brief acceptPoolThreaded Start Web Server as Pool-Threaded (threads are already started and consume clients from a queue)
+     * @param listenerSocket Listener Prepared Socket (Can be TCP, TLS, etc)
+     * @param threadCount Pre-started thread count
+     * @param threadMaxQueuedElements Max queued connections per threads
+     */
+    void acceptPoolThreaded(Network::Streams::StreamSocket * listenerSocket, const uint32_t & threadCount = 20, const uint32_t & threadMaxQueuedElements = 1000 );   
+    /**
+     * @brief setAuthenticator Set the Authenticator for Login
+     * @param value Authenticator Object
+     */
     void setAuthenticator(Authorization::IAuth_Domains *value);
+    /**
+     * @brief setMethodManagers Set the Method Manager that will be used for calling methods
+     * @param value method manager Object
+     */
     void setMethodManagers(MethodsManager *value);
+    /**
+     * @brief setResourceFilter Set the Resource Filter that will be used for managing the access control to static resources
+     * @param value Resource Filter Object
+     */
     void setResourceFilter(ResourcesFilter *value);
-
-    MethodsManager *getMethodManagers() const;
-
-    Authorization::IAuth_Domains *getAuthenticator() const;
-
-    SessionsManager * getSessionsManager();
-
-    bool getUseFormattedJSONOutput() const;
+    /**
+     * @brief setUseFormattedJSONOutput Set This Class Using JSON Formatted Output (multiline) in HTTP Response, default: true.
+     * @param value true for formatted output, false for 1-line json.
+     */
     void setUseFormattedJSONOutput(bool value);
-
-    ResourcesFilter *getResourceFilter() const;
-
-    std::string getResourcesLocalPath() const;
+    /**
+     * @brief setUsingCSRFToken Set CSRF Requirement for JSON API calls (default: true), disable only for non-browser strategies where CSRF is not in your threat model.
+     *
+     *                          - CSRF tokens requires persistent login/session authentication, temporary sessions and methods without csrf won't be admitted.
+     *                          - If you need to have unauthenticated methods (like public tickers), create another web server without CSRF.
+     *                          - CSRF will admit LOGIN without token, and will not allow any method to execute until the confirmation token is sent.
+     *                            This strategy will enable us to avoid using pre-sessions that can be easily starved to Login Denial Of Service.
+     *                            However, starving could happen if the attacker haves valid login credentials.
+     *                          - LOGIN CSRF Confirmation Token should prevent LOGIN CSRF attacks, so the attacker could not pre-establish a foreign session.
+     *                          - Be very careful with "Access-Control-Allow-Origin", bad CORS configuration can create some CSRF vulnerabilities
+     *
+     *                          *** WARNING: NEVER USE NON-CSRF METHODS FOR POSTING/MODIFYING/CHANGING INFO ***
+     * @param value true for CSRF Token ON, any request without the CSRF will be dropped.
+     */
+    void setUsingCSRFToken(bool value);
+    /**
+     * @brief setResourcesLocalPath Set Resources Local Path
+     * @param value Resources Local Path
+     * @return true if path is accessible from this application.
+     */
     bool setResourcesLocalPath(const std::string &value);
+    /**
+     * @brief setExtCallBackOnConnect Set External Callback On Successfully Established Connection
+     * @param value callback details
+     */
+    void setExtCallBackOnConnect(const sWebServerCallBack &value);
+    /**
+     * @brief setExtCallBackOnInitFailed Set External Callback On Protocol Initialization Failture (eg. TLS errors)
+     * @param value callback details
+     */
+    void setExtCallBackOnInitFailed(const sWebServerCallBack &value);
+    /**
+     * @brief setExtCallBackOnTimeOut Set External Callback On Timeout (eg. when connections queue are full and the connection can't be handled)
+     * @param value callback details
+     */
+    void setExtCallBackOnTimeOut(const sWebServerCallBack &value);
+    /**
+     * @brief setObj Set Object passed to method's (void *)
+     * @param value object passed
+     */
+    void setObj(void *value);
+    /**
+     * @brief setSoftwareVersion Set Software Version (to display in `version` RPC method)
+     * @param value version string
+     */
+    void setSoftwareVersion(const std::string &value);
+    /**
+     * @brief setSoftwareVersion
+     * @param major
+     * @param minor
+     * @param subminor
+     */
+    void setSoftwareVersion(const uint32_t major, const uint32_t minor, const uint32_t subminor, const std::string & subText);
+
+    /**
+     * @brief setWebServerName Set Web Server Header for `Server:`
+     * @param value String with the server Name.
+     */
+    void setWebServerName(const std::string &value);
+
+    /**
+     * @brief setUseHTMLIEngine Set Webserver Using HTMLI dynamic in-memory engine (default: true)
+     * @param value true for using, false for not.
+     */
+    void setUseHTMLIEngine(bool value);
+
+    ////////////////////////////////////////////////////////////////////////////////
+    // Internal Methods (ClientHandler->Webserver), don't use them
+    ////////////////////////////////////////////////////////////////////////////////
+    MethodsManager *getMethodManagers() const;
+    Authorization::IAuth_Domains *getAuthenticator() const;
+    SessionsManager * getSessionsManager();
+    ResourcesFilter *getResourceFilter() const;
+    bool getUseFormattedJSONOutput() const;
+    bool getUsingCSRFToken() const;
+    std::string getResourcesLocalPath() const;
+    sWebServerCallBack getExtCallBackOnConnect() const;
+    sWebServerCallBack getExtCallBackOnInitFailed() const;
+    sWebServerCallBack getExtCallBackOnTimeOut() const;
+    std::string getSoftwareVersion() const;
+    std::string getWebServerName() const;
+
+    bool getUseHTMLIEngine() const;
 
 private:
     Network::Sockets::Acceptors::Socket_Acceptor_MultiThreaded multiThreadedAcceptor;
@@ -56,17 +173,19 @@ private:
     /**
      * callback when timed out (all the thread queues are saturated) (this callback is called from acceptor thread, you should use it very quick)
      */
-    static void _callbackOnTimedOut(void *, Network::Streams::StreamSocket *, const char *, bool);
+    static void _callbackOnTimeOut(void *, Network::Streams::StreamSocket *, const char *, bool);
 
     void * obj;
 
+    sWebServerCallBack extCallBackOnConnect, extCallBackOnInitFailed, extCallBackOnTimeOut;
     ResourcesFilter * resourceFilter;
     Authorization::IAuth_Domains * authenticator;
     MethodsManager *methodManagers;
     SessionsManager sessionsManager;
-    bool useFormattedJSONOutput;
+    bool useFormattedJSONOutput, usingCSRFToken, useHTMLIEngine;
     std::string resourcesLocalPath;
-
+    std::string webServerName;
+    std::string softwareVersion;
 };
 
 }}}
