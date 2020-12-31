8aa85c65e6b959dd46b8ed468372cfdd1a564503
Aaron Mizrachi <aaron@unmanarc.com>
2020-12-31 14:25:53 -0600

    String Transform for Authentication Modes

--- cx2Auth/libcx2_auth/src/datastructs/ds_auth_mode.h
+++ cx2Auth/libcx2_auth/src/datastructs/ds_auth_mode.h
@@ -1,6 +1,8 @@
 #ifndef AUTH_MODES_H
 #define AUTH_MODES_H
 
+#include <string>
+
 namespace CX2 { namespace Authentication  {
 
 enum Mode
@@ -8,6 +10,27 @@ enum Mode
     MODE_PLAIN,   // NOT RECOMMENDED.
     MODE_CRAM
 };
+
+static Mode getAuthModeFromString( const std::string & mode )
+{
+    if (mode == "CRAM")
+        return MODE_CRAM;
+    else
+        return MODE_PLAIN;
+}
+
+static std::string getStringFromAuthMode( const Mode & mode )
+{
+    switch (mode)
+    {
+    case MODE_PLAIN:
+        return "PLAIN";
+    case MODE_CRAM:
+        return "CRAM";
+    };
+}
+
+
 
 }}
 
