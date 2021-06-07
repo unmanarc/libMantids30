a5d1ef31d2ed991bb76595f7c16cba1198e67bbc
Aaron Mizrachi <aaron@unmanarc.com>
2021-06-07 01:10:36 -0500

    Comment fix

--- cx2Net/libcx2_net_sockets/src/crypto/cryptostream.h
+++ cx2Net/libcx2_net_sockets/src/crypto/cryptostream.h
@@ -13,7 +13,7 @@ public:
      * @brief mutualChallengeResponseSHA256Auth Create a Mutual Challenge-Reponse Authentication with the remote host
      * @param localKey Local Key
      * @param server Server won't release his hash until client is fully validated. (if both are client, the server will expose a hash that can be bruteforced)
-     * @return pair of booleans, the first value is true if the remote auth suceeed, and the second value is true if the
+     * @return pair of booleans, the first value is true if the remote auth suceeed, and the second value is true if the local challenge succeed.
      */
     std::pair<bool,bool> mutualChallengeResponseSHA256Auth(const std::string & sharedKey, bool server);
 
