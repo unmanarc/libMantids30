43b23bff2e68d5f54799314ae529590aafdde5bf
Aaron Mizrachi <aaron@unmanarc.com>
2021-06-07 01:21:10 -0500

    JSON Simplification

--- cx2RPC/libcx2_xrpc_common/src/authentication.h
+++ cx2RPC/libcx2_xrpc_common/src/authentication.h
@@ -2,7 +2,7 @@
 #define AUTHENTICATION_H
 
 #include <string>
-#include <json/json.h>
+#include <cx2_hlp_functions/json.h>
 namespace CX2 { namespace RPC {
 
 /**
@@ -15,15 +15,16 @@ public:
     Authentication(const std::string & pass, const uint32_t & idx);
 
     bool fromString(const std::string & sAuth);
-    bool fromJSON( const Json::Value & x );
-    Json::Value toJSON() const;
+    bool fromJSON( const json & x );
+    json toJSON() const;
 
     std::string getPassword() const;
     void setPassword(const std::string &value);
 
     uint32_t getPassIndex() const;
     void setPassIndex(const uint32_t &value);
-/*
+
+    /*
     std::string getUserName() const;
     void setUserName(const std::string &value);
 */
