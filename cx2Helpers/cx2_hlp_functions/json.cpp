4569f2d337331944f25c80d88326bf69ea2b1dae
Aaron Mizrachi <aaron@unmanarc.com>
2021-10-20 19:57:15 +0300

    FIX: Helper function impl under namespace

--- cx2Helpers/cx2_hlp_functions/json.cpp
+++ cx2Helpers/cx2_hlp_functions/json.cpp
@@ -1,6 +1,7 @@
 #include "json.h"
 
-std::string jsonToString(const json &value)
+
+std::string CX2::Helpers::jsonToString(const json &value)
 {
     Json::StreamWriterBuilder builder;
     builder.settings_["indentation"] = "";
@@ -13,25 +14,23 @@ std::string jsonToString(const json &value)
     return xstrValue;
 }
 
-using namespace CX2::Helpers;
-
-JSONReader2::JSONReader2()
+CX2::Helpers::JSONReader2::JSONReader2()
 {
     Json::CharReaderBuilder builder;
     reader = builder.newCharReader();
 }
 
-JSONReader2::~JSONReader2()
+CX2::Helpers::JSONReader2::~JSONReader2()
 {
     delete reader;
 }
 
-bool JSONReader2::parse(const std::string &document, Json::Value &root)
+bool CX2::Helpers::JSONReader2::parse(const std::string &document, Json::Value &root)
 {
     return reader->parse(document.c_str(),document.c_str()+document.size(),&root,&errors);
 }
 
-std::string JSONReader2::getFormattedErrorMessages()
+std::string CX2::Helpers::JSONReader2::getFormattedErrorMessages()
 {
     return errors;
 }
