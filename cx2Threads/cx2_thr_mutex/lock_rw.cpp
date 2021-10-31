5433abb903dfa2d74dfb416a5712fd52d295aecd
Aaron Mizrachi <aaron@unmanarc.com>
2021-10-31 22:13:22 +0200

    FIX: logic on R/W mutex

--- cx2Threads/cx2_thr_mutex/lock_rw.cpp
+++ cx2Threads/cx2_thr_mutex/lock_rw.cpp
@@ -6,10 +6,10 @@ Lock_RW::Lock_RW(Mutex_Shared &mutex, bool dontlock)
 {
     this->mutex = &mutex;
     this->dontlock = dontlock;
-    if (dontlock) mutex.lock();
+    if (!dontlock) mutex.lock();
 }
 
 Lock_RW::~Lock_RW()
 {
-    if (dontlock) mutex->unlock();
+    if (!dontlock) mutex->unlock();
 }
