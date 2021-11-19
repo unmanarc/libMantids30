be5229ede594e8454aa08a271f3599c4191afa64
Aaron Mizrachi <aaron@unmanarc.com>
2021-11-19 13:47:33 +0200

    Build Directory

--- build-minsizerel.sh
+++ build-minsizerel.sh
@@ -3,13 +3,13 @@
 PREFIXPATH=/opt/osslibs
 
 prjdir=$(pwd)
-cmake3 . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_INSTALL_LIBDIR=lib -B../cxFramework2-Build
-cd ../cxFramework2-Build
+cmake3 . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/cxFramework2-Build-Shared
+cd ~/builds/cxFramework2-Build-Shared
 make clean
 make -j12 install
 cd "$prjdir"
-cmake3 . -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_LIBDIR=lib -B../cxFramework2-Build-Static
-cd ../cxFramework2-Build-Static
+cmake3 . -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/cxFramework2-Build-Static
+cd ~/builds/cxFramework2-Build-Static
 make clean
 make -j12 install
 cd "$prjdir"
