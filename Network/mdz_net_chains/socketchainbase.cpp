c6b9b49a52d5576ad22c451016d3616ebf5a1acc
Aaron Mizrachi <aaron@unmanarc.com>
2022-06-23 22:20:10 +0300

    Documentation

--- Network/mdz_net_chains/socketchainbase.cpp
+++ Network/mdz_net_chains/socketchainbase.cpp
@@ -21,14 +21,20 @@ bool SocketChainBase::isEndPoint()
 
 std::pair<Mantids::Network::Streams::StreamSocket *, Mantids::Network::Streams::StreamSocket *> SocketChainBase::makeSocketChainPair()
 {
+    // Build 2 sockets from socketpair <--->
     std::pair<Mantids::Network::Streams::StreamSocket *, Mantids::Network::Streams::StreamSocket *> pair = Mantids::Network::Streams::StreamSocket::GetSocketPair();
-    if (pair.first == nullptr) return pair;
 
-    // Transfer the socket from pairsocket to the chained implementation...
+    // IF there is an error (nullptr socket in the pair), return the null pair.
+    if (pair.first == nullptr)
+        return pair;
+
+    // Transfer the socket (sockfd) from the first pairsocket to the chained implementation...
     Mantids::Network::Streams::StreamSocket * realSock = (Mantids::Network::Streams::StreamSocket *)getThis();
     realSock->setSocketFD(pair.first->adquireSocketFD());
     delete pair.first;
     pair.first = realSock;
+
+    // The first from the pair is this class, the second is the connected (pipe/pairsocket)
     return pair;
 }
 
