f27753d46cce00028ea3158f252cd7baae67e59e
Aaron Mizrachi <aaron@unmanarc.com>
2022-08-24 01:22:55 +0300

    NEW: Encrypt/Decrypt using AES-256-GCM encapsulated via base64

--- Helpers/mdz_hlp_functions/crypto.h
+++ Helpers/mdz_hlp_functions/crypto.h
@@ -2,7 +2,9 @@
 #define CRYPTO_H
 
 
+#include <memory>
 #include <string>
+#include "mem.h"
 
 namespace Mantids { namespace Helpers {
 
@@ -11,6 +13,16 @@ class Crypto
 {
 public:
     Crypto();
+
+
+    static std::string AES256EncryptB64(const unsigned char * input, uint32_t inputLen, const char * key, uint32_t keyLen, bool * ok);
+    static std::string AES256EncryptB64(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256EncryptB64(const std::string & input, const std::string & key, bool * ok);
+
+    static std::shared_ptr<Mem::xBinContainer> AES256DecryptB64ToBin(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256DecryptB64(const std::string & input, const char *key, uint32_t keyLen, bool * ok);
+    static std::string AES256DecryptB64(const std::string & input, const std::string & key, bool * ok);
+
     static std::string calcSHA256(const std::string & password);
     static std::string calcSHA512(const std::string & password);
 
