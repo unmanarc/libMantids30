0b7a962c2804054da348a32c35ec02f4fc7380c8
Aaron Mizrachi <aaron@unmanarc.com>
2023-04-25 02:25:52 +0300

    Build using libMantids29

--- build-minsizerel.sh
+++ build-minsizerel.sh
@@ -3,13 +3,13 @@
 PREFIXPATH=/opt/osslibs
 
 prjdir=$(pwd)
-cmake3 . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/libMantids-Build-Shared
-cd ~/builds/libMantids-Build-Shared
+cmake3 . -DBUILD_SHARED_LIBS=ON -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/libMantids29-Build-Shared
+cd ~/builds/libMantids29-Build-Shared
 make clean
 make -j12 install
 cd "$prjdir"
-cmake3 . -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/libMantids-Build-Static
-cd ~/builds/libMantids-Build-Static
+cmake3 . -DCMAKE_INSTALL_PREFIX:PATH=${PREFIXPATH} -DCMAKE_BUILD_TYPE=MinSizeRel -DCMAKE_INSTALL_LIBDIR=lib -B~/builds/libMantids29-Build-Static
+cd ~/builds/libMantids29-Build-Static
 make clean
 make -j12 install
 cd "$prjdir"
