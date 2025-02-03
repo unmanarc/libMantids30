6beabe380d37361c030d766a78d8b8a4a013f0ab
Aaron Mizrachi <aaron@unmanarc.com>
2025-02-04 00:58:56 +0200

    initial basic implementation of weblog

--- Mantids30/Program_Logs/weblog.cpp
+++ Mantids30/Program_Logs/weblog.cpp
@@ -2,7 +2,62 @@
 
 using namespace Mantids30::Program::Logs;
 
-WebLog::WebLog()
-{
+WebLog::WebLog(unsigned int _logMode) : LogBase(_logMode)
+{}
 
+void WebLog::log(
+    const std::string &method, const std::string &url, int statusCode, const std::string &userAgent, const std::string &clientIP)
+{
+    FILE *fp = stdout;
+
+    fprintf(fp, "W/");
+    if (enableDateLogging)
+    {
+        printDate(fp);
+    }
+
+    if (enableColorLogging)
+    {
+        switch (statusCode / 100)
+        {
+        case 2:
+            printColorBold(fp, getAlignedValue("INFO", 12).c_str());
+            break;
+        case 3:
+            printColorBlue(fp, getAlignedValue("REDIRECT", 12).c_str());
+            break;
+        case 4:
+            printColorRed(fp, getAlignedValue("CLIENT_ERROR", 12).c_str());
+            break;
+        case 5:
+            printColorPurple(fp, getAlignedValue("SERVER_ERROR", 12).c_str());
+            break;
+        default:
+            fprintf(fp, "%s", getAlignedValue("UNKNOWN", 12).c_str());
+        }
+    }
+    else
+    {
+        switch (statusCode / 100)
+        {
+        case 2:
+            fprintf(fp, "%s", getAlignedValue("INFO", 12).c_str());
+            break;
+        case 3:
+            fprintf(fp, "%s", getAlignedValue("REDIRECT", 12).c_str());
+            break;
+        case 4:
+            fprintf(fp, "%s", getAlignedValue("CLIENT_ERROR", 12).c_str());
+            break;
+        case 5:
+            fprintf(fp, "%s", getAlignedValue("SERVER_ERROR", 12).c_str());
+            break;
+        default:
+            fprintf(fp, "%s", getAlignedValue("UNKNOWN", 12).c_str());
+        }
+    }
+
+    fprintf(fp, " METHOD=\"%s\" URL=\"%s\" STATUS=%d USER_AGENT=\"%s\" CLIENT_IP=\"%s\"\n", method.c_str(), url.c_str(), statusCode, userAgent.c_str(), clientIP.c_str());
+
+    fflush(stdout);
 }
--- Mantids30/Program_Logs/weblog.h
+++ Mantids30/Program_Logs/weblog.h
@@ -1,12 +1,30 @@
 #pragma once
 
+#include <string>
+#include <ctime>
+
+
+#include "logbase.h"
+
+
 namespace Mantids30 { namespace Program { namespace Logs {
 
-// Under construction...
-class WebLog
+class WebLog : public LogBase
 {
 public:
-    WebLog();
+    WebLog(unsigned int _logMode = MODE_STANDARD);
+
+    void log(const std::string& method,
+                    const std::string& url,
+                    int statusCode,
+                    const std::string& userAgent,
+                    const std::string& clientIP);
+
+
 };
+
+
 }}}
+
+
 
