22ab249d57810c17249b443ea35989f7ac8fcd44
Aaron Mizrachi <dev@unmanarc.com>
2025-05-10 23:37:19 +0300

    NEW: Dummy Socket

---
+++ Mantids30/Net_Sockets/socket_stream_dummy.cpp
@@ -0,0 +1,90 @@
+#include "socket_stream_dummy.h"
+
+using namespace Mantids30::Network::Sockets;
+
+std::string Socket_Stream_Dummy::getPeerName() const
+{
+    return peerName;
+}
+
+void Socket_Stream_Dummy::setPeerName(const std::string &newPeerName)
+{
+    peerName = newPeerName;
+}
+
+bool Socket_Stream_Dummy::isSecure()
+{
+    return true;
+}
+
+
+bool Socket_Stream_Dummy::isConnected()
+{
+    return true;
+}
+
+int Socket_Stream_Dummy::shutdownSocket(
+    int mode)
+{
+    if (mode == SHUT_RDWR)
+    {
+        shuttedDownRD = shuttedDownWR = true;
+    }
+    else if (mode == SHUT_RD)
+    {
+        shuttedDownRD = true;
+    }
+    else if (mode == SHUT_WR)
+    {
+        shuttedDownWR = true;
+    }
+    return 0;
+}
+
+int Socket_Stream_Dummy::partialRead(void *data, const uint32_t &datalen)
+{
+    auto storedDataSize = sender.size();
+    if (!storedDataSize)
+    {
+        return 0;
+    }
+    else if (datalen <= storedDataSize)
+    {
+        // Enviar solo datalen...
+        sender.copyOut(data, datalen);
+        sender.displace(datalen);
+        return datalen;
+    }
+    else
+    {
+        // Send all stored data and clear the container
+        sender.copyOut(data, storedDataSize);
+        sender.clear();
+        return storedDataSize;
+    }
+}
+
+int Socket_Stream_Dummy::partialWrite(const void *data, const uint32_t &datalen)
+{
+    std::pair<bool, uint64_t> r = receiver.append(data, datalen);
+    if (!r.first)
+        return -1;
+    else
+        return r.second;
+}
+
+void Socket_Stream_Dummy::setRemotePairOverride(
+    const char *address)
+{
+    setRemotePair(address);
+}
+
+Mantids30::Memory::Containers::B_Chunks *Socket_Stream_Dummy::getReceiver()
+{
+    return &receiver;
+}
+
+Mantids30::Memory::Containers::B_Chunks *Socket_Stream_Dummy::getSender()
+{
+    return &sender;
+}
---
+++ Mantids30/Net_Sockets/socket_stream_dummy.h
@@ -0,0 +1,117 @@
+#pragma once
+
+/**
+ * @file socket_stream_dummy.h
+ * @brief Header file for Socket_Stream_Dummy class.
+ */
+
+
+#include <Mantids30/Memory/b_chunks.h>
+#include "socket_stream_base.h"
+
+namespace Mantids30 {
+namespace Network {
+namespace Sockets {
+/**
+ * @class Socket_Stream_Dummy
+ * @brief Dummy socket stream implementation.
+ *
+ * This class provides a dummy socket stream implementation for testing and development purposes.
+ */
+class Socket_Stream_Dummy : public Mantids30::Network::Sockets::Socket_Stream_Base
+{
+public:
+    /**
+     * @brief Constructor for Socket_Stream_Dummy.
+     */
+    Socket_Stream_Dummy() = default;
+
+    /**
+     * @brief Get the peer name of the socket.
+     *
+     * @return The peer name as a string.
+     */
+    std::string getPeerName() const override;
+
+    /**
+     * @brief Set the peer name for the socket.
+     *
+     * @param newPeerName The new peer name to set.
+     */
+    void setPeerName(const std::string &newPeerName);
+
+    /**
+     * @brief Check if the socket is secure.
+     *
+     * @return True if the socket is secure, false otherwise. In this dummy implementation, it always returns true.
+     */
+    bool isSecure() override;
+
+    /**
+     * @brief Check if the socket is connected.
+     *
+     * @return True if the socket is connected, false otherwise. In this dummy implementation, it always returns true.
+     */
+    bool isConnected() override;
+
+    /**
+     * @brief Shutdown the socket.
+     *
+     * @param mode The shutdown mode (SHUT_RD, SHUT_WR, or SHUT_RDWR).
+     * @return 0 on success, -1 on error.
+     */
+    int shutdownSocket(int mode = SHUT_RDWR) override;
+
+    /**
+     * @brief Get the sender buffer chunk container.
+     *
+     * @return A pointer to the sender B_Chunks object.
+     */
+    Memory::Containers::B_Chunks *getSender();
+
+    /**
+     * @brief Get the receiver buffer chunk container.
+     *
+     * @return A pointer to the receiver B_Chunks object.
+     */
+    Memory::Containers::B_Chunks *getReceiver();
+
+    /**
+     * @brief Read a data block from the chunk container.
+     *
+     * @param data Pointer to the buffer where the read data will be stored.
+     * @param datalen Maximum number of bytes to read.
+     * @return Number of bytes read, 0 for end of file, or -1 on error.
+     */
+    int partialRead(void *data, const uint32_t &datalen) override;
+
+    /**
+     * @brief Write a data block to the chunk container.
+     *
+     * @param data Pointer to the buffer containing the data to write.
+     * @param datalen Number of bytes to write.
+     * @return Number of bytes written, 0 for end of file, or -1 on error.
+     */
+    int partialWrite(const void *data, const uint32_t &datalen) override;
+
+    /**
+     * @brief Override the remote pair address.
+     *
+     * This function allows overriding the remote pair address with a new one specified by \p address.
+     *
+     * @param address The new remote pair address as a C-style string (null-terminated).
+     */
+    void setRemotePairOverride(const char *address);
+
+
+private:
+    std::string peerName; /**< Peer name associated with the socket. */
+    Memory::Containers::B_Chunks sender;   /**< Sender buffer chunk container. */
+    Memory::Containers::B_Chunks receiver; /**< Receiver buffer chunk container. */
+    bool shuttedDownRD = false;            /**< Flag indicating if the read side has been shutdown. */
+    bool shuttedDownWR = false;            /**< Flag indicating if the write side has been shutdown. */
+};
+
+} // namespace Sockets
+} // namespace Network
+} // namespace Mantids30
