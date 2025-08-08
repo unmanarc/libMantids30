a83dc046b9ec291b207eb649b7e9b33b3c69f2bb
Aaron Mizrachi <dev@unmanarc.com>
2025-08-08 03:22:10 +0300

    Write the transformer EOF at the end of the transmission.
    the streamTo does not send the EOF at the end, just streams to.

--- Mantids30/Memory/streamabletransformer.cpp
+++ Mantids30/Memory/streamabletransformer.cpp
@@ -7,6 +7,8 @@ void Mantids30::Memory::Streams::StreamableTransformer::transform(
     this->destObj = out;
     // Launch to the transformer, and the transformer (this) should perform the transformation to out...
     in->streamTo( this );
+    // Write the EOF
+    this->writeEOF();
     // Reset the dest obj.
     this->destObj = nullptr;
 }
@@ -18,6 +20,9 @@ std::optional<size_t> Mantids30::Memory::Streams::StreamableTransformer::write(c
     if ( count == 0 ) // EOF
     {
         r = writeTransformerEOF(destObj);
+
+        // // Write the EOF after the transform.
+        // destObj->writeEOF();
     }
     else
     {
